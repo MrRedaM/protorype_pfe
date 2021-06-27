3b465240edb2bbcb81462109a56c96413773333b
MrRedaM <r.morsli@esi-sba.dz>
2021-06-27 14:53:30 +0100

    Web export

--- src/scenes/Graph.gd
+++ src/scenes/Graph.gd
@@ -10,3 +10,5 @@ func _on_Graph_connection_request(from, from_slot, to, to_slot):
 	connect_node(from, from_slot, to, to_slot)
 	get_node(from).onConnect(get_node(from), from_slot, get_node(to), to_slot)
 	get_node(to).onConnect(get_node(from), from_slot, get_node(to), to_slot)
+
+
